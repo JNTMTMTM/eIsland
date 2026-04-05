8a18b54a522583eabf84d240fe47a049736b8f30
鸡哥 <15251865468@163.com>
2026-04-05 23:25:04 +0800

    docs: 添加开源信息

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,3 +1,23 @@
+/*
+ * eIsland - A sleek, Apple Dynamic Island inspired floating widget for Windows, built with Electron.
+ * https://github.com/JNTMTMTM/eIsland
+ *
+ * Copyright (C) 2026 JNTMTMTM
+ * Copyright (C) 2026 pyisland.com
+ *
+ * Original author: JNTMTMTM[](https://github.com/JNTMTMTM)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 /**
  * @file index.d.ts
  * @description 渲染进程全局类型声明，扩展 Window 接口以包含 Electron API 和自定义 API
