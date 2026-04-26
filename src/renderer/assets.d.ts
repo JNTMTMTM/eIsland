946af7057c99f70a331451ca0f1811f79e8e40a4
鸡哥 <15251865468@163.com>
2026-04-27 00:16:07 +0800

    feat: 添加wav格式资源声明

--- src/renderer/assets.d.ts
+++ src/renderer/assets.d.ts
@@ -20,7 +20,7 @@
 
 /**
  * @file assets.d.ts
- * @description 渲染进程资源模块类型声明，支持 .jpg/.jpeg/.png/.svg/.gif/.webp 资源导入
+ * @description 渲染进程资源模块类型声明，支持 .jpg/.jpeg/.png/.svg/.gif/.webp/.wav 资源导入
  * @author 鸡哥
  */
 
@@ -50,6 +50,11 @@ declare module '*.gif' {
 }
 
 declare module '*.webp' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.wav' {
   const src: string;
   export default src;
 }
