3cbdde33db51353d79333db8185768ade0fdfc31
鸡哥 <15251865468@163.com>
2026-09-20 23:14:35 +0800

    chore: update vite-env.d.ts with comprehensive licensing and author information

--- web/eisland-web-issue-report/src/vite-env.d.ts
+++ web/eisland-web-issue-report/src/vite-env.d.ts
@@ -1,1 +1,27 @@
+/*
+ * eIsland - A sleek, Apple Dynamic Island inspired floating widget for Windows, built with Electron.
+ * https://github.com/JNTMTMTM/eIsland
+ *
+ * Copyright (C) 2026 JNTMTMTM
+ * Copyright (C) 2026 pyisland.com
+ *
+ * Original author: JNTMTMTM[](https://github.com/JNTMTMTM)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/**
+ * @file vite-env.d.ts
+ * @description Vite 客户端类型声明引用，为 issue-report 子项目提供类型补全。
+ * @author 鸡哥
+ */
+
 /// <reference types="vite/client" />
